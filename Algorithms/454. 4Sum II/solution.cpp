7bb89400092d8a6a85f5e3220ea47070f18a7ee2
saurabh3569 <smishra9109@gmail.com>
2026-02-11 21:58:42 +0530

    new solution added

---
+++ Algorithms/454. 4Sum II/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3,
+                     vector<int>& nums4) {
+        int n = nums1.size();
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = nums1[i] + nums2[j];
+                m[sum]++;
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int target = -(nums3[i] + nums4[j]);
+                ans += m[target];
+            }
+        }
+
+        return ans;
+    }
+};
